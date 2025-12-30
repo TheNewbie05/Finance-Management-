871cf4275a3af63a46a290b959b475f2d6a09cc8
Vishal_01 <141109307+TheNewbie05@users.noreply.github.com>
2025-12-31 00:04:01 +0530

    updated

--- expense.py
+++ expense.py
@@ -1,11 +1,14 @@
-# expense.py
 class Expense:
     def __init__(self, date, category, amount, description):
         self.date = date
         self.category = category
-        self.amount = float(amount)
+        self.amount = amount
         self.description = description
 
-    def to_list(self):
-        """Converts object data into a list for CSV storage."""
-        return [self.date, self.category, self.amount, self.description]
+    def to_dict(self):
+        return {
+            "Date": self.date,
+            "Category": self.category,
+            "Amount": self.amount,
+            "Description": self.description
+        }
